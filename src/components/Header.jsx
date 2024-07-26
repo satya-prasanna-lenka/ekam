154d9e3a28f8e6e77053c6e13aa2d64626b7d671
satya-prasanna-lenka <satyale39@gmail.com>
2024-07-26 23:37:43 +0530

    addingNavigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
 import "../assets/css/header.css";
 const Header = () => {
   return (
     <div className="header">
-      <h3>Wellness Retreats</h3>
+      <h3>
+        <Link style={{ textDecoration: "none", color: "ghostwhite" }} to="/">
+          Wellness Retreats
+        </Link>
+      </h3>
     </div>
   );
 };
